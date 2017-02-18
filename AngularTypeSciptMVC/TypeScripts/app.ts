227b5724ff00588879ebaae61f1c7d699ffb767f
Sy Le Kha <hoctrokha@yahoo.com>
2017-02-18 10:14:35 +0700

    add git ignore for bin/obj

--- AngularTypeSciptMVC/TypeScripts/app.ts
+++ AngularTypeSciptMVC/TypeScripts/app.ts
@@ -1,1 +1,10 @@
-﻿
+﻿import { Component } from '@angular/core';
+
+//@Component({
+//    selector: 'my-app',
+//    template: '<h1>Hello {{name}}</h1>'
+//}).Class(function{
+
+//    });
+
+//export class AppComponent { name = 'Angular'; }
